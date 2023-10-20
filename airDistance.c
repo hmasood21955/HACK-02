a69002a55c95b850cdb5f86abd78494d425bad23
hmasood21955 <hmasood878@gmail.com>
2023-10-20 22:37:10 +0500

    initi

---
+++ airDistance.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+    float location_a,location_b;                    /*phi1 is the latitude of location_a,phi2 is the latitude of location_b */
+    float phi1,phi2,delta;
+    printf("enter value of phi1(latitude of location_a btw |-90-90|) = ");
+    scanf(" %f",&phi1);
+    printf("enter value of phi2(latitude of location_b  btw |-90-90|) = ");
+    scanf(" %f",&phi2);
+    printf("enter value of delta1(logitude of difference |-180-180|) = ");
+    scanf(" %f",&delta);
